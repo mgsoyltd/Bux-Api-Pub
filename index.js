0c69463f062f2f518fa39971f719ab9e32a9ec2b
Erkki Kauhanen <admin@mgsoy.com>
2021-04-11 12:01:45 +0300

    Removed vercel stuff

--- index.js
+++ index.js
@@ -11,12 +11,12 @@ app.use(helmet());
 // Save the app's root directory 
 // global.appRoot = path.resolve(__dirname);
 
-require("../startup/config")();
-require("../startup/cors")(app);
-require("../startup/logging")();
-require("../startup/db")();
-require("../startup/routes")(app);
-require("../startup/validation")();
+require("./startup/config")();
+require("./startup/cors")(app);
+require("./startup/logging")();
+require("./startup/db")();
+require("./startup/routes")(app);
+require("./startup/validation")();
 
 // JUST FOR TESTING UNCAUGHT PROMISE EXCEPTION
 // const p = Promise.reject("Something went down");
