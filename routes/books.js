cb4baf9a7ded56d4074235fabfe119a761941850
Erkki Kauhanen <admin@mgsoy.com>
2021-05-25 19:49:10 +0300

    Changed image storage to Cloudinary

--- routes/books.js
+++ routes/books.js
@@ -155,94 +155,94 @@ router.put("/:id", [validateKey, auth, validateObjectId], async (req, res) => {
 	res.send(book);
 });
 
-/**
- * Update book's image file 
- * 	Endpoint: /<objectid>
- * 	Body form-data: file: <uploadfile>
- */
-router.post("/upload/:id", [validateKey, auth, validateObjectId], async (req, res) => {
+// /**
+//  * Update book's image file 
+//  * 	Endpoint: /<objectid>
+//  * 	Body form-data: file: <uploadfile>
+//  */
+// router.post("/upload/:id", [validateKey, auth, validateObjectId], async (req, res) => {
 
-	if (!req.files || Object.keys(req.files).length === 0) {
-		return res.status(400).send('No files were uploaded.');
-	}
+// 	if (!req.files || Object.keys(req.files).length === 0) {
+// 		return res.status(400).send('No files were uploaded.');
+// 	}
 
-	const fileData = req.files.file;
-	const fileName = fileData.name.toLowerCase();
-	const ext = utils.getExtension(fileName);
-	if (!utils.isImage(fileName)) {
-		return res.status(400).send(`Unsupported image file type: ${ext}`);
-	}
+// 	const fileData = req.files.file;
+// 	const fileName = fileData.name.toLowerCase();
+// 	const ext = utils.getExtension(fileName);
+// 	if (!utils.isImage(fileName)) {
+// 		return res.status(400).send(`Unsupported image file type: ${ext}`);
+// 	}
 
-	if (fileData.truncated === true) {
-		return res.status(400).send(`Image file size (${fileData.size}) exceeds the limit.`);
-	}
+// 	if (fileData.truncated === true) {
+// 		return res.status(413).send(`Image file size (${fileData.size}) exceeds the limit.`);
+// 	}
 
-	let book = await Books.findById(req.params.id).select("-__v");
-	if (!book) {
-		return res.status(404).send("The book with the given ID was not found.");
-	}
+// 	let book = await Books.findById(req.params.id).select("-__v");
+// 	if (!book) {
+// 		return res.status(404).send("The book with the given ID was not found.");
+// 	}
 
-	// Upload an image into img object
-	let contentType = fileData.mimetype;
-	if (contentType == undefined) {
-		contentType = 'image/' + utils.getExtension(fileName);
-	}
+// 	// Upload an image into img object
+// 	let contentType = fileData.mimetype;
+// 	if (contentType == undefined) {
+// 		contentType = 'image/' + utils.getExtension(fileName);
+// 	}
 
-	// console.log(fileData);
-	console.log("filename:", fileName, "extention:", ext, "mimetype:", contentType);
+// 	// console.log(fileData);
+// 	console.log("filename:", fileName, "extention:", ext, "mimetype:", contentType);
 
-	// if (config.get("dbImages")) {
-	// Resize the image and convert to jpeg format
-	const imageData = await sharp(fileData.data)
-		.resize(512)
-		.toFormat("jpeg")
-		.toBuffer();
-	if (imageData) {
-		book.image.data = imageData;
-		book.image.contentType = 'image/jpeg';
-		book.imageURL = "";
-		console.log('Image converted successfully.');
-	}
-	else {
-		return res.status(400).send("Error converting image.");
-	}
-	// }
-	// else {
-	// 	const imagePath = '/public/images/';
-	// 	const imageUrl = `${req.connection.encrypted ? "https" : "http"}://${req.headers.host}/images/${fileName}`;
-	// 	const filePath = global.appRoot + imagePath + fileName;
-	// 	// console.log(imageUrl);
-	// 	// console.log(filePath);
+// 	// if (config.get("dbImages")) {
+// 	// Resize the image and convert to jpeg format
+// 	const imageData = await sharp(fileData.data)
+// 		.resize(512)
+// 		.toFormat("jpeg")
+// 		.toBuffer();
+// 	if (imageData) {
+// 		book.image.data = imageData;
+// 		book.image.contentType = 'image/jpeg';
+// 		book.imageURL = "";
+// 		console.log('Image converted successfully.');
+// 	}
+// 	else {
+// 		return res.status(400).send("Error converting image.");
+// 	}
+// 	// }
+// 	// else {
+// 	// 	const imagePath = '/public/images/';
+// 	// 	const imageUrl = `${req.connection.encrypted ? "https" : "http"}://${req.headers.host}/images/${fileName}`;
+// 	// 	const filePath = global.appRoot + imagePath + fileName;
+// 	// 	// console.log(imageUrl);
+// 	// 	// console.log(filePath);
 
-	// 	// Resize the image and convert to jpeg format
-	// 	sharp(fileData.data)
-	// 		.resize(512)
-	// 		.toFormat("jpeg")
-	// 		.toFile(filePath, (err, info) => {
-	// 			if (err) {
-	// 				return res.status(400).send(err);
-	// 			} else {
-	// 				console.log(`Image uploaded successfully to ${imageUrl}`);
-	// 			}
-	// 		});
+// 	// 	// Resize the image and convert to jpeg format
+// 	// 	sharp(fileData.data)
+// 	// 		.resize(512)
+// 	// 		.toFormat("jpeg")
+// 	// 		.toFile(filePath, (err, info) => {
+// 	// 			if (err) {
+// 	// 				return res.status(400).send(err);
+// 	// 			} else {
+// 	// 				console.log(`Image uploaded successfully to ${imageUrl}`);
+// 	// 			}
+// 	// 		});
 
-	// 	// Update the imageURL on the book
-	// 	book.imageURL = imageUrl;
-	// }
+// 	// 	// Update the imageURL on the book
+// 	// 	book.imageURL = imageUrl;
+// 	// }
 
-	book.save((error, savedBook) => {
-		if (error) {
-			return res.status(404).send(error.details[0].message);
-		}
-		// if (config.get("dbImages")) {
-		return res.status(200).send('Image uploaded successfully.');
-		// }
-		// else {
-		// 	return res.status(200).send(`${imageUrl}`);
-		// }
-	});
+// 	book.save((error, savedBook) => {
+// 		if (error) {
+// 			return res.status(404).send(error.details[0].message);
+// 		}
+// 		// if (config.get("dbImages")) {
+// 		return res.status(200).send('Image uploaded successfully.');
+// 		// }
+// 		// else {
+// 		// 	return res.status(200).send(`${imageUrl}`);
+// 		// }
+// 	});
 
-});
+// });
 
 /**
  * Delete a book
